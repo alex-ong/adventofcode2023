f79e53684e3ddb30596ba8d2bfe7f81d113b15e7
Alex Ong <alex.ong@unsw.edu.au>
2023-12-04 17:29:09 +1100

    refactor: put classes in their own file

---
+++ day3/classes.py
@@ -0,0 +1,115 @@
+import re
+from dataclasses import dataclass
+
+NON_PART = "123456789."
+NUMBER_REGEX = r"\d+"
+
+
+@dataclass
+class PartNumber:
+    col: int
+    row: int
+    length: int
+    value: int
+
+    def touching(self, col, row, row_size):
+        """returns if a given coordinate is touching this PartNumber"""
+        start_x = max(0, self.col - 1)
+        end_x = min(self.end_index, row_size)
+
+        if not start_x <= col <= end_x:
+            return False
+        if not self.row - 1 <= row <= self.row + 1:
+            return False
+        return True
+
+    @property
+    def end_index(self):
+        return self.col + self.length
+
+
+@dataclass
+class Gear:
+    col: int
+    row: int
+
+    part_numbers: list[PartNumber] = None
+
+    @property
+    def gear_ratio(self):
+        """If we have exactly two parts, returns the gear ratio"""
+        if self.part_numbers is None:
+            raise ValueError("self.words not initialized")
+        if len(self.part_numbers) == 2:
+            return self.part_numbers[0].value * self.part_numbers[1].value
+        return 0  # or None..
+
+
+@dataclass
+class Matrix:
+    data: list[str]
+
+    @property
+    def row_size(self) -> int:
+        return len(self.data[0])
+
+    @property
+    def row_count(self) -> int:
+        return len(self.data)
+
+    def get_part_numbers(self) -> list[PartNumber]:
+        """Retrieve numbered words like 456 from the matrix"""
+        results = []
+
+        for row, line in enumerate(self.data):
+            matches = re.finditer(NUMBER_REGEX, line)
+            for match in matches:
+                start, end = match.start(), match.end()
+                value = int(line[start:end])
+                part_number = PartNumber(
+                    row=row, col=start, length=end - start, value=value
+                )
+                results.append(part_number)
+        return results
+
+    def is_engine_part(self, part_number):
+        """return whether a word is an engine part by looking at its surroundings"""
+        start_x = max(0, part_number.col - 1)
+        end_x = min(part_number.end_index + 1, self.row_size)
+
+        if part_number.row >= 1:  # row above
+            for char in self.data[part_number.row - 1][start_x:end_x]:
+                if char not in NON_PART:
+                    return True
+        if part_number.row < self.row_count - 1:  # row below
+            for char in self.data[part_number.row + 1][start_x:end_x]:
+                if char not in NON_PART:
+                    return True
+        if part_number.col >= 1:  # left one
+            if self.data[part_number.row][part_number.col - 1] not in NON_PART:
+                return True
+        if part_number.end_index < self.row_size - 1:  # right one
+            if self.data[part_number.row][part_number.end_index] not in NON_PART:
+                return True
+
+        return False
+
+    def get_gears(self, part_numbers: list[PartNumber]):
+        """Retrieve gears from the matrix"""
+        results = []
+        for row, line in enumerate(self.data):
+            for col, char in enumerate(line):
+                if char == "*":
+                    gear = Gear(col=col, row=row)
+                    gear.part_numbers = self.find_gear_parts(gear, part_numbers)
+                    results.append(gear)
+        return results
+
+    def find_gear_parts(self, gear: Gear, part_numbers: list[PartNumber]):
+        if gear.part_numbers is not None:
+            return gear.part_numbers
+        result = []
+        for part_number in part_numbers:
+            if part_number.touching(gear.col, gear.row, self.row_size):
+                result.append(part_number)
+        return result
--- day3/day3a.py
+++ day3/day3a.py
@@ -2,122 +2,12 @@
 Day 3 implementation
 """
 import re
-from dataclasses import dataclass
 
-NON_PART = "123456789."
-
-
-@dataclass
-class Matrix:
-    data: list[str]
-
-    @property
-    def row_size(self) -> int:
-        return len(self.data[0])
-
-    @property
-    def row_count(self) -> int:
-        return len(self.data)
-
-
-@dataclass
-class Word:
-    col: int
-    row: int
-    length: int
-    value: int
-
-    def touching(self, col, row, row_size):
-        start_x = max(0, self.col - 1)
-        end_x = min(self.end_index, row_size)
-
-        if not start_x <= col <= end_x:
-            return False
-        if not self.row - 1 <= row <= self.row + 1:
-            return False
-        return True
-
-    @property
-    def end_index(self):
-        return self.col + self.length
-
-    def is_engine_part(self, matrix):
-        """row above"""
-        row_size, num_rows = matrix.row_size, matrix.row_count
-        start_x = max(0, self.col - 1)
-        end_x = min(self.end_index + 1, row_size)
-        data = matrix.data
-        if self.row >= 1:  # row above
-            for char in data[self.row - 1][start_x:end_x]:
-                if char not in NON_PART:
-                    return True
-        if self.row < num_rows - 1:  # row below
-            for char in data[self.row + 1][start_x:end_x]:
-                if char not in NON_PART:
-                    return True
-        if self.col >= 1:  # left one
-            if data[self.row][self.col - 1] not in NON_PART:
-                return True
-        if self.end_index < row_size - 1:  # right one
-            if data[self.row][self.end_index] not in NON_PART:
-                return True
-
-        return False
-
-
-@dataclass
-class Gear:
-    col: int
-    row: int
-
-    words: list[Word] = None
-
-    @property
-    def gear_ratio(self):
-        if self.words is None:
-            raise ValueError("self.words not intialized")
-        if len(self.words) == 2:
-            return self.words[0].value * self.words[1].value
-        return 0  # or None..
-
-    def find_words(self, words: list[Word], matrix):
-        if self.words is not None:
-            return self.words
-        result = []
-        for word in words:
-            if word.touching(self.col, self.row, matrix.row_size):
-                result.append(word)
-        self.words = result
-        return result
-
-
-NUMBER_REGEX = r"\d+"
-
-
-def get_words(matrix: Matrix):
-    results = []
-
-    for row, line in enumerate(matrix.data):
-        matches = re.finditer(NUMBER_REGEX, line)
-        for match in matches:
-            start, end = match.start(), match.end()
-            value = int(line[start:end])
-            word = Word(row=row, col=start, length=end - start, value=value)
-            results.append(word)
-    return results
-
-
-def get_gears(matrix: Matrix):
-    results = []
-    for row, line in enumerate(matrix.data):
-        for col, char in enumerate(line):
-            if char == "*":
-                gear = Gear(col=col, row=row)
-                results.append(gear)
-    return results
+from classes import Gear, Matrix, PartNumber
 
 
 def get_data() -> Matrix:
+    """Convert text file to matrix"""
     with open("data.txt", "r", encoding="utf8") as file:
         data = file.readlines()
         data = [line.strip() for line in data]
@@ -126,19 +16,15 @@ def get_data() -> Matrix:
 
 def main():
     matrix = get_data()
-    words = get_words(matrix)
-    part_filter = lambda x: x.is_engine_part(matrix)
-    words = list(filter(part_filter, words))
+    part_numbers = matrix.get_part_numbers()
+    part_filter = lambda part_number: matrix.is_engine_part(part_number)
+    part_numbers = list(filter(part_filter, part_numbers))
+
     # q1
-    print(sum([word.value for word in words]))
+    print(sum([part_number.value for part_number in part_numbers]))
+
     # q2
-    gears = get_gears(matrix)
-
-    for gear in gears:
-        gear.find_words(words, matrix)
-        if len(gear.words) != 2:
-            print(gear)
-
+    gears: list[Gear] = matrix.get_gears(part_numbers)
     print(sum(gear.gear_ratio for gear in gears))
 
 
