92392c6780ead9b4f9843dadf56f490e23c62793
alexo <the.onga@gmail.com>
2023-12-04 23:06:40 +1100

    feat: add day1 and day2

---
+++ day1/day1a.py
@@ -0,0 +1,17 @@
+import os
+
+with open("test.txt", 'r', encoding="utf8") as file:
+    total = 0
+    for line in file:
+        first = None
+        last = None
+        for char in line:
+            if char.isnumeric():
+                if first is None:
+                    first = char
+                last = char
+        print(line.strip())
+        data = int(first+last)
+        
+        total += data
+    print(total)
---
+++ day1/day1b.py
@@ -0,0 +1,58 @@
+import os
+
+class IndexValue:
+    index = 0
+    value = "1"
+
+    
+    def __init__(self, index, value):
+        self.index = index
+        self.value = value
+        
+    def __str__(self):
+        return f"(i:{self.index}, v:{self.value})"
+        
+
+class WordNumber:
+    word = "one"
+    number = "1"
+    
+    def __init__(self, word, number):
+        self.word = word
+        self.number = number
+        
+mappings = [
+    WordNumber("one", "1"),
+    WordNumber("two", "2"),
+    WordNumber("three", "3"),
+    WordNumber("four", "4"),
+    WordNumber("five", "5"),
+    WordNumber("six", "6"),
+    WordNumber("seven", "7"),
+    WordNumber("eight", "8"),
+    WordNumber("nine", "9"),
+]
+
+    
+with open("test.txt", 'r', encoding="utf8") as file:
+    total = 0
+    for line in file:
+        index_to_chars = {}
+        for index, char in enumerate(line):
+            if char.isnumeric():
+                index_to_chars[index] = char
+            
+            for mapping in mappings:
+                substring = line[index:index+len(mapping.word)]
+                if substring == mapping.word:
+                    index_to_chars[index] = mapping.number
+                    
+        print(index_to_chars, line)
+        first_index = min(index_to_chars.keys())
+        last_index = max(index_to_chars.keys())
+        
+        number = int(index_to_chars[first_index] + index_to_chars[last_index])
+        total += number
+        
+        
+    print(total)
