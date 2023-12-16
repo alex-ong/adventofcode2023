721d49a5f72b457cac2e635e109cb96ec37309d4
alexo <the.onga@gmail.com>
2023-12-17 01:04:35 +1100

    day13: part2

--- day13/day13.py
+++ day13/day13.py
@@ -1,74 +1,26 @@
 """day13 solution"""
 
-from dataclasses import dataclass, field
-import itertools
-from math import floor, ceil
+from dataclasses import dataclass
+
+
+def distance(left: str, right: str) -> int:
+    """returns edit distance of two strings"""
+    return sum(a != b for a, b in zip(left, right))
 
 
 @dataclass
 class Maze:
     tiles: list[str]
 
-    row_reflect: float | None = field(init=False, default=None)
-    col_reflect: float | None = field(init=False, default=None)
+    def solve(self, distance=0):
+        row_reflect = self.reflect_rows(distance)
+        col_reflect = self.reflect_cols(distance)
+        return self.score(row_reflect, col_reflect)
 
-    @property
-    def num_cols(self):
-        return len(self.tiles[0])
+    def reflect_rows(self, distance):
+        return self.check_reflection(self.tiles, distance)
 
-    @property
-    def num_rows(self):
-        return len(self.tiles)
-
-    def __post_init__(self):
-        """post setup"""
-        self.reflect_rows()
-        self.reflect_cols()
-
-    def __str__(self):
-        if self.col_reflect is not None:
-            result = self.col_number_header() + self.col_arrow_header()
-            result += "\n".join(str(tile) for tile in self.tiles) + "\n"
-            result += self.col_arrow_header() + self.col_number_header()
-            result += f"col: {self.col_reflect+1}"
-        elif self.row_reflect is not None:
-            result = ""
-            numbers = self.get_numbers(self.num_rows)
-            for index, line in enumerate(self.tiles):
-                pointer = " "
-                if index == floor(self.row_reflect):
-                    pointer = "v"
-                elif index == ceil(self.row_reflect):
-                    pointer = "^"
-                result += (
-                    numbers[index]
-                    + pointer
-                    + str(line)
-                    + " "
-                    + pointer
-                    + numbers[index]
-                    + "\n"
-                )
-            result += f"row: {self.row_reflect+1}"
-        return result
-
-    def get_numbers(self, max_number):
-        return "".join(itertools.islice(itertools.cycle("1234567890"), max_number))
-
-    def col_number_header(self):
-        if self.col_reflect is not None:
-            return self.get_numbers(self.num_cols) + "\n"
-        return ""
-
-    def col_arrow_header(self):
-        if self.col_reflect is not None:
-            return " " * floor(self.col_reflect) + "><" + "\n"
-        return ""
-
-    def reflect_rows(self):
-        self.row_reflect = self.check_reflection(self.tiles)
-
-    def reflect_cols(self):
+    def reflect_cols(self, distance):
         """checks reflection of cols by flipping rows/cols"""
         cols: list[list[str]] = [[] for _ in range(len(self.tiles[0]))]
         for row in self.tiles:
@@ -76,41 +28,31 @@ class Maze:
                 cols[col_index].append(col)
 
         cols_strs = ["".join(str(col)) for col in cols]
-        self.col_reflect = self.check_reflection(cols_strs)
+        return self.check_reflection(cols_strs, distance)
 
-    def check_reflection(self, data: list[str]):
+    def check_reflection(self, data: list[str], target_dist: int):
         """Checks a reflection on rows or cols"""
-        mirror_candidates = []
-        for index, row in enumerate(data):
-            if index == len(data) - 1:
-                break
-            if row == data[index + 1]:
-                mirror_candidates.append(index + 0.5)
+        for index in range(len(data)):
+            if index == 0:
+                continue
 
-        if len(mirror_candidates) == 0:
-            return None
+            top_blocks = reversed(data[:index])
+            bottom_blocks = data[index:]
+            pairs = zip(top_blocks, bottom_blocks)  # accounts for shorter list
+            total_diff = sum(distance(top, bottom) for top, bottom in pairs)
 
-        for mirror_candidate in mirror_candidates:
-            works = True
-            low_index, high_index = floor(mirror_candidate), ceil(mirror_candidate)
-            while low_index >= 0 and high_index < len(data):
-                if data[low_index] != data[high_index]:
-                    works = False
-                    break
-                low_index -= 1
-                high_index += 1
-            if works:
-                return mirror_candidate
+            if total_diff == target_dist:
+                return index
 
         return None
 
-    def score(self):
+    def score(self, row_reflect, col_reflect) -> int:
         """returns score for q1"""
-        if self.col_reflect is not None:
-            return floor(self.col_reflect + 1)
-        elif self.row_reflect is not None:
-            return 100 * floor(self.row_reflect + 1)
-        return 0
+        if col_reflect is not None:
+            return col_reflect
+        if row_reflect is not None:
+            return 100 * row_reflect
+        raise ValueError("expected a mirror!")
 
 
 def read_input() -> list[Maze]:
@@ -133,10 +75,11 @@ def read_input() -> list[Maze]:
 def main():
     mazes: list[Maze] = read_input()
 
-    for maze in mazes:
-        print(maze)
     # q1
-    print(sum(maze.score() for maze in mazes))
+    print(sum(maze.solve(0) for maze in mazes))
+
+    # q2
+    print(sum(maze.solve(distance=1) for maze in mazes))
 
 
 if __name__ == "__main__":
