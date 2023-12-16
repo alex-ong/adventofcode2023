7c4604f903c220d74cfd5856e70eb3f37cd745ca
alexo <the.onga@gmail.com>
2023-12-17 03:06:24 +1100

    day14: part2

--- day14/day14.py
+++ day14/day14.py
@@ -27,6 +27,8 @@ class World:
     data: Any
     left_is: Direction = Direction.West
 
+    score: int | None = None
+
     def rotate_world_cw(self) -> "World":
         rotated = list(zip(*self.data[::-1]))
         return World(rotated, self.left_is.rotate_cw())
@@ -39,11 +41,45 @@ class World:
         return hash(str(self.data) + ":" + str(self.left_is))
 
     def print_map(self):
+        """prints the map nicely"""
         print("\n".join(str(row) for row in self.data))
-        print(self.left_is)
+
+    def print_correct(self):
+        """Pirnt with north facing up"""
+        world = self.correct_side()
+        world.print_map()
+        print(f"last action:{self.left_is}")
+
+    def get_score(self):
+        world = self.correct_side()
+        return naive_score(world.data)
+
+    def correct_side(self) -> "World":
+        world = self
+        if world.left_is == Direction.West:
+            return self
+        elif world.left_is == Direction.North:
+            return world.rotate_world_cw()
+        elif world.left_is == Direction.East:
+            world = world.rotate_world_cw()
+            world = world.rotate_world_cw()
+            return world
+        elif world.left_is == Direction.South:
+            return world.rotate_world_ccw()
+        raise ValueError(f"Unsupported Direction: {world.left_is}")
 
 
-def simulate_row(row) -> tuple[list[str], int]:
+def naive_score(world_rows):
+    """returns score assuming west is pointing left"""
+    num_rows = len(world_rows)
+    score = 0
+    for index, row in enumerate(world_rows):
+        o_count = row.count("O")
+        score += o_count * (num_rows - index)
+    return score
+
+
+def simulate_row(row):
     """simulates a row; returns its value and the new row"""
     square_indices = [-1] + [i for i, x in enumerate(row) if x == "#"] + [len(row)]
     pairs = zip(square_indices, square_indices[1:])
@@ -64,12 +100,59 @@ def simulate_row(row) -> tuple[list[str], int]:
     return list(new_row), row_score
 
 
+def simulate_world(world_rows):
+    """simulates world by rolling to the left."""
+    result = []
+    for world_row in world_rows:
+        result.append(simulate_row(world_row)[0])
+    return result
+
+
 def get_input():
     """grabs input, rotated so that left is north"""
-    with open("input-arrow.txt") as file:
+    with open("input.txt") as file:
         world = list(file)
 
     return World(world)
+
+
+def question2(world: World):
+    cache: dict[World, int] = {}
+    cycle_results: list[World] = []
+    for cycle in range(10000):
+        # left_is: north
+        world = world.rotate_world_cw()
+        world.data = simulate_world(world.data)
+
+        # left is: west
+        world = world.rotate_world_cw()
+        world.data = simulate_world(world.data)
+
+        # left is: south
+        world = world.rotate_world_cw()
+        world.data = simulate_world(world.data)
+        world.score = world.get_score()
+        # world.print_correct()
+        print(cycle, world.score)
+        # east here
+        if world in cache:
+            cycle_start = cache[world]
+            cycle_end = cycle
+            break
+        cache[world] = cycle
+        cycle_results.append(world)
+
+        # left is east
+        world = world.rotate_world_cw()
+        world.data = simulate_world(world.data)
+
+    cycle_length = cycle_end - cycle_start
+    print(f"cycle length: {cycle_length}")
+
+    final_target = 1000000000
+    target = (final_target - cycle_start - 1) % cycle_length
+    result = cycle_results[cycle_start + target]
+    return result.score
 
 
 def main():
@@ -81,14 +164,8 @@ def main():
     # q1:
     print(sum(simulate_row(row)[1] for row in world.data))
 
-    # q2:
-    world.print_map()
-    world = world.rotate_world_cw()
-    world.print_map()
-    world = world.rotate_world_cw()
-    world.print_map()
-    world = world.rotate_world_cw()
-    world.print_map()
+    world.data = simulate_world(world.data)
+    print(question2(world))
 
 
 if __name__ == "__main__":
