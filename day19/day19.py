0aee75703aaa2040740a1b2711268fd0c617514f
alex-ong <the.onga@gmail.com>
2023-12-19 22:26:21 +1100

    day19: part2

--- day19/day19.py
+++ day19/day19.py
@@ -1,4 +1,6 @@
-from lib.classes import Part, Workflow
+from queue import Queue
+
+from lib.classes import Part, PartRange, PartRangeDest, Workflow
 from lib.parsers import parse_part_string, parse_workflow_string
 
 """
@@ -9,7 +11,7 @@ parsing classes section
 def get_input() -> tuple[list[Workflow], list[Part]]:
     workflows: list[Workflow] = []
     parts: list[Part] = []
-    with open("input-small.txt", encoding="utf8") as file:
+    with open("input.txt", encoding="utf8") as file:
         for line in file:
             if len(line.strip()) == 0:
                 break
@@ -35,15 +37,43 @@ def process_part(workflows: dict[str, Workflow], part: Part) -> bool:
         workflow = workflows[workflow_name]
 
 
-def main() -> None:
-    workflows, parts = get_input()
+def solve_part2(workflows: dict[str, Workflow]) -> None:
+    min_xmas = Part(1, 1, 1, 1)
+    max_xmas = Part(4001, 4001, 4001, 4001)
+    part_range = PartRange(min_xmas, max_xmas)
 
+    starting_condition = PartRangeDest(part_range, "in")
+
+    to_process: Queue[PartRangeDest] = Queue()
+    to_process.put(starting_condition)
+    result = 0
+    while not to_process.empty():
+        part_range_dest = to_process.get()
+        part_range, dest = part_range_dest.part_range, part_range_dest.destination
+
+        to_add: list[PartRangeDest] = workflows[dest].process_part_range(part_range)
+        for item in to_add:
+            if item.destination == "A":
+                result += item.part_range.size()
+            elif item.destination != "R":
+                to_process.put(item)
+    print(result)
+
+
+def main() -> None:
+    # combined
+    workflows, parts = get_input()
     workflows_mapping: dict[str, Workflow] = {wf.name: wf for wf in workflows}
+
+    # part 1
     total = 0
     for part in parts:
         if process_part(workflows_mapping, part):
             total += part.rating
     print(total)
+
+    # part 2
+    solve_part2(workflows_mapping)
 
 
 if __name__ == "__main__":
--- day19/lib/classes.py
+++ day19/lib/classes.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from enum import StrEnum
+from typing import Optional
 
 
 @dataclass
@@ -13,12 +14,95 @@ class Part:
     def rating(self) -> int:
         return sum([self.x, self.m, self.a, self.s])
 
+    def clone_modify(self, component: "Component", value: int) -> "Part":
+        x, m, a, s = self.x, self.m, self.a, self.s
+        if component == Component.X:
+            x = value
+        if component == Component.M:
+            m = value
+        if component == Component.A:
+            a = value
+        if component == Component.S:
+            s = value
+
+        return Part(x, m, a, s)
+
+    def get_value(self, component: "Component") -> int:
+        if component == Component.X:
+            return self.x
+        if component == Component.M:
+            return self.m
+        if component == Component.A:
+            return self.a
+        if component == Component.S:
+            return self.s
+        raise ValueError(f"Unsupported component{component}")
+
 
 class Component(StrEnum):
     X = "x"
     M = "m"
     A = "a"
     S = "s"
+
+
+@dataclass
+class PartRange:
+    min_values: Part  # from
+    max_values: Part  # to, non-inclusive
+
+    def size(self) -> int:
+        return (
+            (self.max_values.x - self.min_values.x)
+            * (self.max_values.m - self.min_values.m)
+            * (self.max_values.a - self.min_values.a)
+            * (self.max_values.s - self.min_values.s)
+        )
+
+    def split(
+        self, component: Component, split_value: int
+    ) -> tuple[Optional["PartRange"], Optional["PartRange"]]:
+        """
+        Split a partrange in two, using a chosen component and splitvalue
+        in the case that our range falls on one wohle side, we return None.
+        E.g.
+        range = 0-100; split == 200 -> return [(0-100), None]
+        range = 100-200; split == 50 -> return [None, (100-200)]
+        range = 100-200, split == 150 -> return [(100-150), (150-200)]
+        """
+        min_value = self.min_values.get_value(component)
+        max_value = self.max_values.get_value(component)
+        if split_value >= max_value:
+            return (self, None)
+        if split_value < min_value:
+            return (None, self)
+
+        mid_high = self.min_values.clone_modify(component, split_value)
+        mid_low = self.max_values.clone_modify(component, split_value)
+
+        return (
+            PartRange(self.min_values, mid_low),
+            PartRange(mid_high, self.max_values),
+        )
+
+    def __str__(self) -> str:
+        return ", ".join(
+            [
+                f"{self.min_values.x}<=x<={self.max_values.x-1}",
+                f"{self.min_values.m}<=m<={self.max_values.m-1}",
+                f"{self.min_values.a}<=a<={self.max_values.a-1}",
+                f"{self.min_values.s}<=s<={self.max_values.s-1}",
+            ]
+        )
+
+
+@dataclass
+class PartRangeDest:
+    part_range: PartRange
+    destination: str
+
+    def __str__(self) -> str:
+        return self.destination + ":" + str(self.part_range)
 
 
 class Comparator(StrEnum):
@@ -52,6 +136,17 @@ class Condition:
         else:
             raise ValueError(f"Unsupported comparator: {self.sign}")
 
+    def process_part_range(
+        self, part_range: PartRange
+    ) -> tuple[Optional[PartRange], Optional[PartRange]]:
+        """return pass, fail as ranges"""
+        if self.sign == Comparator.LessThan:
+            success, fail = part_range.split(self.component, self.value)
+            return (success, fail)
+        if self.sign == Comparator.GreaterThan:
+            fail, success = part_range.split(self.component, self.value + 1)
+        return (success, fail)
+
 
 @dataclass
 class Rule:
@@ -67,6 +162,19 @@ class Rule:
             return self.destination
         return None
 
+    def process_part_range(
+        self, part_range: PartRange
+    ) -> tuple[Optional[PartRangeDest], Optional[PartRange]]:
+        success: Optional[PartRange]
+        fail: Optional[PartRange]
+        if self.condition is None:
+            success, fail = part_range, None
+        else:
+            success, fail = self.condition.process_part_range(part_range)
+        if success is not None:
+            return (PartRangeDest(success, self.destination), fail)
+        return None, fail
+
 
 @dataclass
 class Workflow:
@@ -80,3 +188,20 @@ class Workflow:
             if destination is not None:
                 return destination
         raise ValueError("uh oh, hit the end of worfkflow!")
+
+    def process_part_range(self, part_range: PartRange) -> list[PartRangeDest]:
+        """
+        Follow rule list. Each success has to branch off,
+        each failure continues down the chain.
+        """
+        results: list[PartRangeDest] = []
+        remainder: Optional[PartRange] = part_range
+
+        for rule in self.rules:
+            if remainder is None:
+                break
+            success, remainder = rule.process_part_range(remainder)
+            if success is not None:
+                results.append(success)
+
+        return results
