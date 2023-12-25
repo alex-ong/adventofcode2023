346b2b84d5e4a47f8d889582b3530bcc50c802cd
alexo <the.onga@gmail.com>
2023-12-25 20:04:56 +1100

    day16: multiprocessing

--- day16/day16.py
+++ day16/day16.py
@@ -3,6 +3,10 @@ day16 solution
 """
 
 
+import os
+
+from tqdm.contrib.concurrent import process_map
+
 from day16.lib.direction import Direction
 from day16.lib.laser import Laser
 from day16.lib.parsers import get_input
@@ -17,6 +21,12 @@ def solve_task(task: Laser, world: World) -> int:
     return world.solve(task).num_energized()
 
 
+def solve_task_wrapper(args: tuple[Laser, World]) -> int:
+    """Wraps solve_task in multiprocessing, since it only takes one arg"""
+    task, world = args
+    return solve_task(task, world)
+
+
 def part1(world: World) -> int:
     laser = Laser(0, 0, Direction.EAST)
     solved_world = world.solve(laser)
@@ -26,15 +36,22 @@ def part1(world: World) -> int:
 def part2(world: World) -> int:
     tasks = []
     # part 2: brute force coz our impl is already cached/backtracked
+    # 1T -> 3.3s
+    # 12T -> 1.1s
     for col in range(world.num_cols):
-        tasks.append(Laser(0, col, Direction.SOUTH))
-        tasks.append(Laser(world.num_rows - 1, col, Direction.NORTH))
+        tasks.append((Laser(0, col, Direction.SOUTH), world))
+        tasks.append((Laser(world.num_rows - 1, col, Direction.NORTH), world))
 
     for row in range(world.num_rows):
-        tasks.append(Laser(row, 0, Direction.EAST))
-        tasks.append(Laser(row, world.num_cols - 1, Direction.WEST))
+        tasks.append((Laser(row, 0, Direction.EAST), world))
+        tasks.append((Laser(row, world.num_cols - 1, Direction.WEST), world))
 
-    return max(solve_task(task, world) for task in tasks)
+    cpu_count = os.cpu_count() or 1
+    chunk_size = (len(tasks) // cpu_count) + 1
+    results: list[int]
+    results = process_map(solve_task_wrapper, tasks, chunksize=chunk_size)  # type: ignore
+
+    return max(results)
 
 
 def main() -> None:
