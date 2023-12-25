d782efc7bea40edcaa68bcf87c6a8f335e450a6d
alexo <the.onga@gmail.com>
2023-12-25 17:32:32 +1100

    day25: done

---
+++ day25/day25.py
@@ -0,0 +1,68 @@
+"""day25 solution"""
+from dataclasses import dataclass
+
+import matplotlib.pyplot as plt
+import networkx as nx
+
+INPUT_SMALL = "day25/input-small.txt"
+INPUT = "day25/input.txt"
+
+
+@dataclass
+class Connection:
+    src: str
+    dests: list[str]
+
+    def node_names(self) -> list[str]:
+        return [self.src] + self.dests
+
+
+def parse_connection(line: str) -> Connection:
+    src, dests = line.split(":")
+    return Connection(src, dests.split())
+
+
+def get_data(path: str) -> list[Connection]:
+    connections: list[Connection] = []
+    with open(path, "r", encoding="utf8") as file:
+        for line in file:
+            connections.append(parse_connection(line))
+
+    return connections
+
+
+def show_graph(graph: nx.Graph) -> None:
+    """Draws a graph that you can see"""
+    nx.draw(graph, with_labels=True)
+    plt.draw()
+    plt.show()
+
+
+def graph_nodes(connections: list[Connection]) -> int:
+    """Graphs the modules"""
+    G = nx.Graph()
+
+    nodes: set[str] = set()
+    for connection in connections:
+        to_add = connection.node_names()
+        for node_name in to_add:
+            if node_name not in nodes:
+                nodes.add(node_name)
+                G.add_node(node_name)
+            if node_name != connection.src:
+                G.add_edge(connection.src, node_name)
+
+    show_graph(G)
+    cut_value, partition = nx.stoer_wagner(G)
+    print(f"num_cuts: {cut_value}")
+    return len(partition[0]) * len(partition[1])
+
+
+def main() -> None:
+    conns = get_data(INPUT)
+    result = graph_nodes(conns)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
