ce7a412ab885325576832eea9a0aa953a3d0c4c4
alexo <the.onga@gmail.com>
2023-12-25 14:51:21 +1100

    feat: add lib/tests folder generator

---
+++ maker.py
@@ -0,0 +1,16 @@
+import os
+from pathlib import Path
+from typing import Iterable
+
+
+def touch_days(days: Iterable[int]) -> None:
+    for day in days:
+        os.makedirs(f"day{day:02}/lib", exist_ok=True)
+        os.makedirs(f"day{day:02}/tests", exist_ok=True)
+        Path(f"day{day:02}/tests/__init__.py").touch()
+        Path(f"day{day:02}/__init__.py").touch()
+        Path(f"day{day:02}/lib/__init__.py").touch()
+
+
+if __name__ == "__main__":
+    touch_days(range(1, 26))
