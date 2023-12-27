7f699f42e539017e9682adb78aef8df9fd3e79ed
alexo <the.onga@gmail.com>
2023-12-27 11:46:09 +1100

    day07: coverage

--- day07/tests/test_day7.py
+++ day07/tests/test_day7.py
@@ -1,3 +1,5 @@
+import pytest
+
 from day07.day7 import INPUT_SMALL, Hand, HandPart2, calculate_hands, parse_lines
 
 
@@ -21,3 +23,29 @@ def test_hand() -> None:
     assert hand2 < hand1
     assert hand1 > hand2
     assert hand1 == hand3
+
+    # mypy actually stops us from doing `hand1 < 1`
+    # So we disable it so we can test our raise,
+    # in case a dev decides to do the comparison
+    with pytest.raises(ValueError):
+        hand1 < 1  # type: ignore[operator]
+
+    # here we don't need to disable it, since ruff
+    # tells us to use __eq__(self, other:object) -> bool:
+    # This means we do expect people to use hand1 == 6 and
+    # we want to throw that error
+    with pytest.raises(ValueError):
+        hand1 == 6
+
+    data = [
+        ("32T3K", 765),
+        ("T55J5", 684),
+        ("KK677", 28),
+        ("KTJJT", 220),
+        ("QQQJA", 483),
+        ("JJJJJ", 483),
+    ]
+
+    hands = (HandPart2(cards, bet) for cards, bet in data)
+    oaks = [hand.calculate_of_a_kind() for hand in hands]
+    assert [oak[0] for oak in oaks] == [2, 4, 2, 4, 4, 5]
