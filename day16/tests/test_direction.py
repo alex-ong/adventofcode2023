618f3a85106688b3f02ca07cc6996a30e943d512
alex-ong <the.onga@gmail.com>
2023-12-30 16:04:01 +1100

    test: day16

--- day16/tests/test_direction.py
+++ day16/tests/test_direction.py
@@ -13,3 +13,5 @@ def test_direction() -> None:
     assert Direction.WEST.offset(0, 0) == (0, -1)
     assert Direction.NORTH.offset(0, 0) == (-1, 0)
     assert Direction.SOUTH.offset(0, 0) == (1, 0)
+
+    assert str(Direction.WEST) == "WEST"
